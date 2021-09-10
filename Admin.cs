0d27596b38186e0ba92ce68865f771d239b137bd
Sekfiser <sekfiser@gmail.com>
2021-09-10 19:11:51 +0800

    Добавьте файлы проекта.

---
+++ Admin.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+namespace Lab1
+{
+    class Admin : User
+    {
+        /// <summary>
+        /// Count of wrong password inputs.
+        /// </summary>
+        public int PasswordInputCount { get; private set; }
+        /// <summary>
+        /// State of users account. True means blocked.
+        /// </summary>
+        public bool Blocked { get; private set; }
+
+        /// <summary>
+        /// New administrator creation.
+        /// </summary>
+        /// <param name="login">Login to account</param>
+        /// <param name="password">Password to account</param>
+       
+        public Admin(string login, string password) : base(login, password) 
+        {
+            PasswordInputCount = 0;
+            Blocked = false;
+        }
+        /// <summary>
+        /// Method to check entered password.
+        /// </summary>
+        /// <param name="password">Inputed password</param>
+        /// <returns>Returns true if password correct, returns false in other cases. Will block user account in case of 3 wrong inputs.</returns>
+        public override bool PassCheck(string password)
+        {
+            if (Blocked == false && password.Equals(Password))
+            {
+                PasswordInputCount = 0;
+                return true;
+            }
+            else
+            {
+                if (Blocked == true)
+                {
+                    Console.WriteLine("Your account blocked!");
+                    return false;
+                }
+                else
+                {
+                    PasswordInputCount++;
+                    if (PasswordInputCount >= 3)
+                    {
+                        Blocked = true;
+                    }
+                    return false;
+                }
+            }
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Lab1
+{
+    /// <summary>
+    /// Abstract class user
+    /// </summary>
+    abstract class User
+    {
+        /// <summary>
+        /// User login
+        /// </summary>
+        public string Login { get; private set; }
+        /// <summary>
+        /// User password
+        /// </summary>
+        public string Password { get; private set; }
+        /// <summary>
+        /// New user creation.
+        /// </summary>
+        /// <param name="login">Login to users account</param>
+        /// <param name="password">Password to user account</param>
+        public User(string login, string password) 
+        {
+            Login = login;
+            Password = password;
+        }
+        /// <summary>
+        /// Abstract method to check entered password
+        /// </summary>
+        /// <param name="password">Inputed password</param>
+        /// <returns>Returns true if password correct, returns false in other cases.</returns>
+        abstract public bool PassCheck(string password); 
+    }
+}
